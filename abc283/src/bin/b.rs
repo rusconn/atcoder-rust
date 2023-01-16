a0914406f00c788b47879f9985fd5feebe7f9236
rusconn <rusconn3@gmail.com>
2023-01-16 11:14:05 +0000

    AC abc283_b

--- abc283/src/bin/b.rs
+++ abc283/src/bin/b.rs
@@ -1,13 +1,35 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: u32,
+        mut a: [u32; n],
+        q: u32,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&mut a, q));
 }
 
-fn solve(n: i32) -> impl std::fmt::Display {
-    n
+fn solve(a: &mut [u32], q: u32) -> impl std::fmt::Display {
+    (0..q)
+        .filter_map(|_| {
+            input! {
+                num: u32,
+                k: usize,
+            }
+
+            if num == 1 {
+                input! {
+                    x: u32,
+                }
+
+                a[k - 1] = x;
+
+                None
+            } else {
+                Some(a[k - 1])
+            }
+        })
+        .join("\n")
 }
