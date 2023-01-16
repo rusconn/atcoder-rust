9cde6f28edba350cb7e6b46c9b5afea6fc1d750d
rusconn <rusconn3@gmail.com>
2023-01-16 16:08:27 +0000

    Refine abc282_b

--- abc282/src/bin/b.rs
+++ abc282/src/bin/b.rs
@@ -14,10 +14,8 @@ fn main() {
 fn solve(ss: &[String]) -> impl std::fmt::Display {
     ss.iter()
         .combinations(2)
-        .filter(|v| {
-            let x = v[0];
-            let y = v[1];
-
+        .map(|v| (v[0], v[1]))
+        .filter(|(x, y)| {
             x.chars()
                 .zip(y.chars())
                 .all(|(xc, yc)| xc == 'o' || yc == 'o')
