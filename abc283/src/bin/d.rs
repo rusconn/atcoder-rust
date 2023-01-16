30aaa255f589c848c297c45f738404ac411ec774
rusconn <rusconn3@gmail.com>
2023-01-16 13:48:54 +0000

    AC abc283_d

--- abc283/src/bin/d.rs
+++ abc283/src/bin/d.rs
@@ -1,13 +1,57 @@
+use std::collections::HashSet;
+
+use maplit::hashset;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        s: String,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&s));
 }
 
-fn solve(n: i32) -> impl std::fmt::Display {
-    n
+fn solve(s: &str) -> impl std::fmt::Display {
+    yesno(can(s))
+}
+
+fn yesno(b: bool) -> &'static str {
+    if b {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+fn can(s: &str) -> bool {
+    let mut box_: HashSet<char> = hashset! {};
+    let mut stack: Vec<_> = vec![];
+    let mut current = hashset! {};
+
+    for c in s.chars() {
+        match c {
+            '(' => {
+                stack.push(current);
+                current = hashset! {};
+            }
+            ')' => {
+                // in place な diff API が無い？
+                for c2 in current {
+                    box_.remove(&c2);
+                }
+
+                current = stack.pop().unwrap();
+            }
+            _ => {
+                if box_.contains(&c) {
+                    return false;
+                }
+
+                box_.insert(c);
+                current.insert(c);
+            }
+        }
+    }
+
+    true
 }
