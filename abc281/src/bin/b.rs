af1bbc92fbe0a9739f247c487a258c68f826ed83
rusconn <rusconn3@gmail.com>
2023-01-25 14:38:10 +0000

    AC abc281_b

--- abc281/src/bin/b.rs
+++ abc281/src/bin/b.rs
@@ -2,12 +2,31 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        s: String,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&s));
 }
 
-fn solve(n: i32) -> impl std::fmt::Display {
-    n
+fn solve(s: &str) -> impl std::fmt::Display {
+    if sandwich(s) {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+fn sandwich(s: &str) -> bool {
+    if s.len() != 8 {
+        return false;
+    }
+
+    let mut chars = s.chars();
+    let head = chars.next().unwrap();
+    let mut body = chars.collect::<String>();
+    let tail = body.pop().unwrap();
+
+    head.is_ascii_uppercase()
+        && tail.is_ascii_uppercase()
+        && body.parse::<u32>().map_or(false, |n| n >= 100000)
 }
