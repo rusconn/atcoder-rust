c8771bcdfd94cc439df171e837ab52e2485d88c7
rusconn <rusconn3@gmail.com>
2023-01-25 13:49:21 +0000

    AC abc281_a

--- abc281/src/bin/a.rs
+++ abc281/src/bin/a.rs
@@ -1,13 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: u8,
     }
 
     println!("{}", solve(n));
 }
 
-fn solve(n: i32) -> impl std::fmt::Display {
-    n
+fn solve(n: u8) -> impl std::fmt::Display {
+    (0..=n).rev().join("\n")
 }
