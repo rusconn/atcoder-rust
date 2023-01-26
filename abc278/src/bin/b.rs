2df33973b053cad6ae4835baef1d1daac2a2dad9
rusconn <rusconn3@gmail.com>
2023-01-26 10:19:57 +0000

    WA abc278_b

--- abc278/src/bin/b.rs
+++ abc278/src/bin/b.rs
@@ -4,12 +4,26 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        h: u8,
+        m: u8,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(h, m));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(h: u8, m: u8) -> impl fmt::Display {
+    let minutes = u16::from(h) * 60 + u16::from(m);
+    let minutes = (minutes..).find(|&m| is_misleading(m)).unwrap();
+
+    format!("{} {}", minutes / 60, minutes % 60)
+}
+
+fn is_misleading(minutes: u16) -> bool {
+    let h = minutes / 60;
+    let m = minutes % 60;
+
+    let new_h = h / 10 * 10 + m / 10;
+    let new_m = h % 10 * 10 + m % 10;
+
+    new_h < 24 && new_m < 60
 }
