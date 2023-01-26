04c35a679898691c821bba08083f937f99b8bfbb
rusconn <rusconn3@gmail.com>
2023-01-26 11:53:26 +0000

    WA abc278_c

--- abc278/src/bin/c.rs
+++ abc278/src/bin/c.rs
@@ -1,15 +1,47 @@
 use std::fmt;
 
 use proconio::input;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 fn main() {
     input! {
-        n: i32,
+        _: u32,
+        q: u32,
+        tabs: [(u8, usize, usize); q],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&tabs));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(tabs: &[(u8, usize, usize)]) -> impl fmt::Display {
+    let mut follows: FxHashMap<usize, FxHashSet<usize>> = FxHashMap::default();
+    let mut answers: Vec<&str> = vec![];
+
+    for &(t, a, b) in tabs {
+        match t {
+            1 => {
+                follows
+                    .entry(a)
+                    .or_insert_with(FxHashSet::default)
+                    .insert(b);
+            }
+            2 => {
+                follows
+                    .entry(b)
+                    .or_insert_with(FxHashSet::default)
+                    .remove(&a);
+            }
+            _ => {
+                if follows.get(&a).map_or(false, |s| s.contains(&b))
+                    && follows.get(&b).map_or(false, |s| s.contains(&a))
+                {
+                    answers.push("Yes");
+                } else {
+                    answers.push("No");
+                }
+            }
+        }
+    }
+
+    answers.join("\n")
 }
