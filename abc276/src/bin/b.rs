eae85af3661b7d4e21860b397860ca528a5d4235
rusconn <rusconn3@gmail.com>
2023-01-27 10:47:06 +0000

    AC abc276_b

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -2,41 +2,36 @@ use std::collections::BTreeSet;
 
 use itertools::Itertools;
 use proconio::{fastout, input};
+use rustc_hash::FxHashMap;
 
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        n: u32,
         m: u32,
     }
 
-    let mut roads_map: Vec<BTreeSet<_>> = Vec::with_capacity(n + 1);
-
-    unsafe {
-        roads_map.set_len(n + 1);
-    }
+    let mut roads_map = FxHashMap::default();
 
     for _ in 0..m {
         input! {
-            a: usize,
-            b: usize,
+            a: u32,
+            b: u32,
         }
 
-        if let Some(set) = roads_map.get_mut(a) {
-            set.insert(b);
-        } else {
-            roads_map[a] = BTreeSet::default();
-        }
+        roads_map
+            .entry(a)
+            .or_insert_with(BTreeSet::default)
+            .insert(b);
 
-        if let Some(set) = roads_map.get_mut(b) {
-            set.insert(a);
-        } else {
-            roads_map[b] = BTreeSet::default();
-        }
+        roads_map
+            .entry(b)
+            .or_insert_with(BTreeSet::default)
+            .insert(a);
     }
 
     for i in 1..=n {
-        if let Some(roads) = roads_map.get(i) {
+        if let Some(roads) = roads_map.get(&i) {
             println!("{} {}", roads.len(), roads.iter().join(" "));
         } else {
             println!(0);
