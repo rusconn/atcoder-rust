d8eca567a5010adcccee064f7c185b7033a131d7
rusconn <rusconn3@gmail.com>
2023-01-27 03:37:02 +0000

    AC abc277_b

--- abc277/src/bin/b.rs
+++ abc277/src/bin/b.rs
@@ -1,15 +1,33 @@
 use std::fmt;
 
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
+
+const SUITS: [char; 4] = ['H', 'D', 'C', 'S'];
+
+const RANKS: [char; 13] = [
+    'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K',
+];
 
 fn main() {
     input! {
-        n: i32,
+        n: u8,
+        ss: [Chars; n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&ss));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(ss: &[Vec<char>]) -> impl fmt::Display {
+    if is_playing_cards(ss) {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+fn is_playing_cards(ss: &[Vec<char>]) -> bool {
+    ss.iter()
+        .all(|s| SUITS.contains(&s[0]) && RANKS.contains(&s[1]))
+        && ss.iter().tuple_combinations().all(|(s1, s2)| s1 != s2)
 }
