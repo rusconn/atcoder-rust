b089a48d9f65a09a79035d4d3ee4523a1615d6d3
rusconn <rusconn3@gmail.com>
2023-01-27 10:58:52 +0000

    Refine abc276_b

--- abc276/src/bin/b.rs
+++ abc276/src/bin/b.rs
@@ -2,39 +2,31 @@ use std::collections::BTreeSet;
 
 use itertools::Itertools;
 use proconio::{fastout, input};
-use rustc_hash::FxHashMap;
 
 #[fastout]
 fn main() {
     input! {
-        n: u32,
+        n: usize,
         m: u32,
     }
 
-    let mut roads_map = FxHashMap::default();
+    let mut roads_map = vec![BTreeSet::default(); n];
 
     for _ in 0..m {
         input! {
-            a: u32,
-            b: u32,
+            a: usize,
+            b: usize,
         }
 
-        roads_map
-            .entry(a)
-            .or_insert_with(BTreeSet::default)
-            .insert(b);
-
-        roads_map
-            .entry(b)
-            .or_insert_with(BTreeSet::default)
-            .insert(a);
+        roads_map[a - 1].insert(b);
+        roads_map[b - 1].insert(a);
     }
 
-    for i in 1..=n {
-        if let Some(roads) = roads_map.get(&i) {
-            println!("{} {}", roads.len(), roads.iter().join(" "));
-        } else {
+    for roads in roads_map {
+        if roads.is_empty() {
             println!(0);
+        } else {
+            println!("{} {}", roads.len(), roads.iter().join(" "));
         }
     }
 }
