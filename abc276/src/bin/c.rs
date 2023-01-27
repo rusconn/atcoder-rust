5627cde1854bf04cb126f9c36caf5f7bb067dc1f
rusconn <rusconn3@gmail.com>
2023-01-27 13:59:32 +0000

    Refine abc276_c

--- abc276/src/bin/c.rs
+++ abc276/src/bin/c.rs
@@ -1,65 +1,16 @@
-use std::fmt;
+use proconio::{fastout, input};
+use superslice::Ext;
 
-use itertools::Itertools;
-use proconio::input;
-
+#[fastout]
 fn main() {
     input! {
         n: u8,
         mut ps: [u8; n],
     }
 
-    println!("{}", solve(&mut ps));
-}
+    ps.prev_permutation();
 
-fn solve(ps: &mut [u8]) -> impl fmt::Display {
-    let mut i = 0;
-
-    for j in 1..ps.len() {
-        if is_sorted(&ps[j..]) {
-            break;
-        }
-        i = j;
+    for p in ps {
+        print!("{} ", p);
     }
-
-    let j = pred_index(&ps, i);
-
-    ps.swap(i, j);
-
-    let (l, r) = ps.split_at_mut(i + 1);
-
-    r.reverse();
-
-    l.iter_mut().chain(r).join(" ")
-}
-
-fn is_sorted(xs: &[u8]) -> bool {
-    if xs.is_empty() {
-        return true;
-    }
-
-    let mut pred = xs[0];
-
-    for &x in xs.iter().skip(1) {
-        if pred > x {
-            return false;
-        }
-        pred = x;
-    }
-
-    true
-}
-
-fn pred_index(xs: &[u8], i: usize) -> usize {
-    let mut pred = 0;
-    let mut pred_i = 0;
-
-    for j in i + 1..xs.len() {
-        if xs[i] > xs[j] && pred < xs[j] {
-            pred = xs[j];
-            pred_i = j;
-        }
-    }
-
-    pred_i
 }
