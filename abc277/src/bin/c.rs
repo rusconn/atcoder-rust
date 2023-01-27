b05f92906a1bc51cac1ae4fc0ea9a1bbb9f07df3
rusconn <rusconn3@gmail.com>
2023-01-27 05:04:18 +0000

    WA TLE abc277_c

--- abc277/src/bin/c.rs
+++ abc277/src/bin/c.rs
@@ -1,15 +1,30 @@
 use std::fmt;
 
 use proconio::input;
+use rustc_hash::FxHashSet;
 
 fn main() {
     input! {
-        n: i32,
+        n: u32,
+        abs: [(u32, u32); n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&abs));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(abs: &[(u32, u32)]) -> impl fmt::Display {
+    let mut top = 0;
+    let mut visited = FxHashSet::default();
+    let mut remaining = vec![1];
+
+    while let Some(x) = remaining.pop() {
+        top = top.max(x);
+        visited.insert(x);
+
+        abs.iter()
+            .filter(|&(a, b)| *a == x && !visited.contains(b))
+            .for_each(|&(_, b)| remaining.push(b));
+    }
+
+    top
 }
