04916f18e38684d873966be8478c2fe80dc81e2d
rusconn <rusconn3@gmail.com>
2023-01-28 01:03:08 +0000

    AC abc275_a

--- abc275/src/bin/a.rs
+++ abc275/src/bin/a.rs
@@ -4,12 +4,13 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: u8,
+        hs: [u32; n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&hs));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(hs: &[u32]) -> impl fmt::Display {
+    hs.iter().enumerate().max_by_key(|p| p.1).unwrap().0 + 1
 }
