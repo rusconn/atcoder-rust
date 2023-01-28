a1fe63977b6c9c9adfa0ce17f828d844d0d350fa
rusconn <rusconn3@gmail.com>
2023-01-28 01:47:35 +0000

    AC abc275_b

--- abc275/src/bin/b.rs
+++ abc275/src/bin/b.rs
@@ -2,6 +2,8 @@ use std::fmt;
 
 use proconio::input;
 
+const DIVISOR: u128 = 998244353;
+
 fn main() {
     input! {
         abcdef: [u128; 6]
@@ -11,6 +13,8 @@ fn main() {
 }
 
 fn solve(abcdef: &[u128]) -> impl fmt::Display {
-    let rs: Vec<_> = abcdef.iter().map(|x| x % 998244353).collect();
-    (rs[0] * rs[1] * rs[2] - rs[3] * rs[4] * rs[5]) % 998244353
+    let rs: Vec<_> = abcdef.iter().map(|x| x % DIVISOR).collect();
+    let abc = rs[0] * rs[1] * rs[2] % DIVISOR;
+    let def = rs[3] * rs[4] * rs[5] % DIVISOR;
+    (abc + DIVISOR - def) % DIVISOR
 }
