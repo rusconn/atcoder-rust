de0a77744d45b8c072bedbbe0b9813920926965f
rusconn <rusconn3@gmail.com>
2023-01-28 04:29:31 +0000

    Fastify abc274_b

--- abc274/src/bin/b.rs
+++ abc274/src/bin/b.rs
@@ -1,20 +1,20 @@
 use std::fmt;
 
 use itertools::Itertools;
-use proconio::{input, marker::Chars};
+use proconio::{input, marker::Bytes};
 
 fn main() {
     input! {
         h: usize,
         w: usize,
-        css: [Chars; h],
+        css: [Bytes; h],
     }
 
     println!("{}", solve(w, &css));
 }
 
-fn solve(w: usize, css: &[Vec<char>]) -> impl fmt::Display {
+fn solve(w: usize, css: &[Vec<u8>]) -> impl fmt::Display {
     (0..w)
-        .map(|j| css.iter().filter(|cs| cs[j] == '#').count())
+        .map(|j| css.iter().filter(|cs| cs[j] == b'#').count())
         .join(" ")
 }
