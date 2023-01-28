d89839b865f1b106a5b1356a95eb7eb66e5d9f1d
rusconn <rusconn3@gmail.com>
2023-01-28 01:06:54 +0000

    Refine abc275_a

--- abc275/src/bin/a.rs
+++ abc275/src/bin/a.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
@@ -12,5 +13,5 @@ fn main() {
 }
 
 fn solve(hs: &[u32]) -> impl fmt::Display {
-    hs.iter().enumerate().max_by_key(|p| p.1).unwrap().0 + 1
+    hs.iter().position_max().unwrap() + 1
 }
