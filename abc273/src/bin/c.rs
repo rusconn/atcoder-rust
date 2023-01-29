43b94aa823ecb577d62f287acc1918a5159f5d37
rusconn <rusconn3@gmail.com>
2023-01-29 13:37:29 +0000

    AC abc273_c

--- abc273/src/bin/c.rs
+++ abc273/src/bin/c.rs
@@ -1,15 +1,28 @@
 use std::fmt;
 
+use itertools::Itertools;
 use proconio::input;
+use rustc_hash::FxHashMap;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        a: [u32; n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(n, &a));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(n: usize, a: &[u32]) -> impl fmt::Display {
+    let mut histogram = FxHashMap::default();
+
+    for &x in a {
+        *histogram.entry(x).or_insert(0) += 1;
+    }
+
+    let greaters = a.iter().sorted().rev().dedup().collect_vec();
+
+    (0..n)
+        .map(|k| greaters.get(k).map_or(0, |v| *histogram.get(v).unwrap()))
+        .join("\n")
 }
