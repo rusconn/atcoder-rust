ee727ecb3ac1cf278c59f4d0764b2756f5913b22
rusconn <rusconn3@gmail.com>
2023-01-29 11:45:12 +0000

    AC abc273_a

--- abc273/src/bin/a.rs
+++ abc273/src/bin/a.rs
@@ -1,15 +1,16 @@
 use std::fmt;
 
+use permutohedron::factorial;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
     }
 
     println!("{}", solve(n));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(n: usize) -> impl fmt::Display {
+    factorial(n)
 }
