d7c119050979b3e4d7a8d6fedcb0cbf3765ebf8b
rusconn <rusconn3@gmail.com>
2023-01-30 13:20:36 +0000

    AC abc272_d

--- abc272/src/bin/d.rs
+++ abc272/src/bin/d.rs
@@ -16,24 +16,24 @@ fn main() {
 fn solve(n: usize, m: usize) -> impl fmt::Display {
     let mut counts = vec![vec![-1; n]; n];
     let mut arrivals = VecDeque::default();
+    let mut moves = vec![];
 
     counts[0][0] = 0i16;
     arrivals.push_back((0usize, 0usize));
 
+    for y in 0..=m.sqrt() {
+        let rest = m - y.pow(2);
+        let x = rest.sqrt();
+        if y.pow(2) + x.pow(2) == m {
+            moves.push((y, x));
+            moves.push((x, y));
+        }
+    }
+
     while let Some((i, j)) = arrivals.pop_front() {
         let next_count = counts[i][j] + 1;
-        let mut moves = vec![];
 
-        for y in 0..=m.sqrt() {
-            let rest = m - y.pow(2);
-            if rest.is_power_of_two() {
-                let x = rest.sqrt();
-                moves.push((y, x));
-                moves.push((x, y));
-            }
-        }
-
-        for (y, x) in moves {
+        for &(y, x) in &moves {
             let mut next_coords = vec![];
 
             if y <= i && x <= j {
