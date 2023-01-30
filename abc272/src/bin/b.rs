b848fd56acc7cda8eab79acec374c86b7950141f
rusconn <rusconn3@gmail.com>
2023-01-30 05:03:41 +0000

    AC abc272_b

--- abc272/src/bin/b.rs
+++ abc272/src/bin/b.rs
@@ -1,15 +1,45 @@
 use std::fmt;
 
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        m: usize,
     }
 
-    println!("{}", solve(n));
+    let mut xss = vec![];
+
+    for _ in 0..m {
+        input! {
+            k: u8,
+            xs: [Usize1; k],
+        }
+
+        xss.push(xs);
+    }
+
+    println!("{}", solve(n, &xss));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(n: usize, xss: &[Vec<usize>]) -> impl fmt::Display {
+    if is_all_friends(n, xss) {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+fn is_all_friends(n: usize, xss: &[Vec<usize>]) -> bool {
+    let mut vs = vec![vec![false; n]; n];
+
+    for xs in xss {
+        for &x in xs {
+            for &y in xs {
+                vs[x][y] = true;
+            }
+        }
+    }
+
+    vs.iter().all(|v| v.iter().all(|&b| b))
 }
