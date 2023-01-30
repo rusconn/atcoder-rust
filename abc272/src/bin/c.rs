f8d4ee84feac9ec0aa612a7c3e91378c47f7babf
rusconn <rusconn3@gmail.com>
2023-01-30 06:11:08 +0000

    AC abc272_c

--- abc272/src/bin/c.rs
+++ abc272/src/bin/c.rs
@@ -1,15 +1,39 @@
-use std::fmt;
+use std::{cmp::Reverse, fmt};
 
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: u32,
+        a: [i32; n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&a));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(a: &[i32]) -> impl fmt::Display {
+    if let Some(x) = max_even(a) {
+        x
+    } else {
+        -1
+    }
+}
+
+fn max_even(a: &[i32]) -> Option<i32> {
+    let (mut evens, mut odds): (Vec<i32>, Vec<i32>) = a.iter().partition(|&&ai| ai % 2 == 0);
+
+    evens.sort_unstable_by_key(|&x| Reverse(x));
+    odds.sort_unstable_by_key(|&x| Reverse(x));
+
+    let mut max_even = None;
+
+    if let (Some(x), Some(y)) = (evens.get(0), evens.get(1)) {
+        max_even = Some(x + y);
+    }
+
+    if let (Some(x), Some(y)) = (odds.get(0), odds.get(1)) {
+        max_even = max_even.max(Some(x + y));
+    }
+
+    max_even
 }
