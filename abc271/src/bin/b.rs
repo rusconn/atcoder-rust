4f2579d2be128f7e76e1c6920893a6c84fcab734
rusconn <rusconn3@gmail.com>
2023-01-31 04:49:36 +0000

    AC abc271_b

--- abc271/src/bin/b.rs
+++ abc271/src/bin/b.rs
@@ -1,15 +1,39 @@
 use std::fmt;
 
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        q: usize,
     }
 
-    println!("{}", solve(n));
+    let mut ass = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        input! {
+            l: u32,
+            a: [u32; l],
+        }
+
+        ass.push(a);
+    }
+
+    let mut sts = Vec::with_capacity(q);
+
+    for _ in 0..q {
+        input! {
+            s: Usize1,
+            t: Usize1,
+        }
+
+        sts.push((s, t));
+    }
+
+    println!("{}", solve(&ass, &sts));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(ass: &[Vec<u32>], sts: &[(usize, usize)]) -> impl fmt::Display {
+    sts.iter().map(|&(s, t)| ass[s][t]).join("\n")
 }
