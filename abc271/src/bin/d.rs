0398e4f0d100e2f1f2c6adf6f5683b37d52d6530
rusconn <rusconn3@gmail.com>
2023-01-31 14:02:09 +0000

    RE abc271_d

--- abc271/src/bin/d.rs
+++ abc271/src/bin/d.rs
@@ -1,15 +1,63 @@
 use std::fmt;
 
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        s: usize,
+        abs: [(usize, usize); n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(n, s, &abs));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(n: usize, s: usize, abs: &[(usize, usize)]) -> impl fmt::Display {
+    if let Some(th) = is_adjustable(n, s, abs) {
+        format!("Yes\n{}", th)
+    } else {
+        "No".to_string()
+    }
+}
+
+fn is_adjustable(n: usize, s: usize, abs: &[(usize, usize)]) -> Option<String> {
+    let mut dp = vec![vec![false; s + 1]; n + 1];
+
+    dp[1][abs[0].0] = true;
+    dp[1][abs[0].1] = true;
+
+    for (i, &(a, b)) in abs.iter().enumerate().skip(1) {
+        for j in 1..=s {
+            if dp[i][j] {
+                if j + a <= s {
+                    dp[i + 1][j + a] = true;
+                }
+                if j + b <= s {
+                    dp[i + 1][j + b] = true;
+                }
+            }
+        }
+    }
+
+    if !dp[n][s] {
+        return None;
+    }
+
+    let mut ths = vec![];
+    let mut j = s;
+
+    for (i, &(a, b)) in abs.iter().enumerate().skip(1).rev() {
+        if dp[i][j - a] {
+            ths.push('H');
+            j -= a;
+        } else {
+            ths.push('T');
+            j -= b;
+        }
+    }
+
+    ths.push(if j == abs[0].0 { 'H' } else { 'T' });
+
+    Some(ths.iter().rev().join(""))
 }
