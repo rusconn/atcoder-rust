e8c4e2ecbdbe72ece16f8b006043a81186ad9e8b
rusconn <rusconn3@gmail.com>
2023-01-31 06:27:28 +0000

    WA TLE abc271_c

--- abc271/src/bin/c.rs
+++ abc271/src/bin/c.rs
@@ -4,12 +4,35 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: u32,
+        mut a: [u32; n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&mut a));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(a: &mut [u32]) -> impl fmt::Display {
+    a.sort_unstable();
+
+    let mut p = 0;
+    let mut i = 0;
+    let mut j = a.len() - 1;
+
+    while i <= j {
+        if p + 1 == a[i] {
+            i += 1;
+            p += 1;
+            continue;
+        }
+
+        if j - i >= 1 {
+            j -= 2;
+            p += 1;
+            continue;
+        }
+
+        break;
+    }
+
+    p
 }
