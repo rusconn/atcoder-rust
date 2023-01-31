61d16312c3f5cd777641844dc1af6134ad333a14
rusconn <rusconn3@gmail.com>
2023-01-31 05:07:12 +0000

    Refine abc271_b

--- abc271/src/bin/b.rs
+++ abc271/src/bin/b.rs
@@ -5,30 +5,10 @@ use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-        n: usize,
-        q: usize,
-    }
-
-    let mut ass = Vec::with_capacity(n);
-
-    for _ in 0..n {
-        input! {
-            l: u32,
-            a: [u32; l],
-        }
-
-        ass.push(a);
-    }
-
-    let mut sts = Vec::with_capacity(q);
-
-    for _ in 0..q {
-        input! {
-            s: Usize1,
-            t: Usize1,
-        }
-
-        sts.push((s, t));
+        n: u32,
+        q: u32,
+        ass: [[u32]; n],
+        sts: [(Usize1, Usize1); q],
     }
 
     println!("{}", solve(&ass, &sts));
