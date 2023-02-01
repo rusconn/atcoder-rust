89eefb111842d340e02d88e3509bfba8ebac963f
rusconn <rusconn3@gmail.com>
2023-02-01 06:18:44 +0000

    Refine abc270_b

--- abc270/src/bin/b.rs
+++ abc270/src/bin/b.rs
@@ -12,34 +12,24 @@ fn main() {
     println!("{}", solve(x, y, z));
 }
 
-fn solve(x: i16, y: i16, z: i16) -> impl fmt::Display {
+fn solve(mut x: i16, mut y: i16, mut z: i16) -> impl fmt::Display {
     if x < 0 {
-        if y < x || 0 < y {
-            return -x;
-        }
-
-        if z < y {
-            return -1;
-        }
-
-        if z < 0 {
-            return -x;
-        }
-
-        2 * z - x
-    } else {
-        if y < 0 || x < y {
-            return x;
-        }
-
-        if y < z {
-            return -1;
-        }
-
-        if 0 < z {
-            return x;
-        }
-
-        -2 * z + x
+        x = -x;
+        y = -y;
+        z = -z;
     }
+
+    if y < 0 || x < y {
+        return x;
+    }
+
+    if y < z {
+        return -1;
+    }
+
+    if 0 < z {
+        return x;
+    }
+
+    -2 * z + x
 }
