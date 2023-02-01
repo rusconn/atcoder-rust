23f7964c818e5cdb1301a7ac5e3692abde77ad81
rusconn <rusconn3@gmail.com>
2023-02-01 13:27:33 +0000

    AC abc269_b

--- abc269/src/bin/b.rs
+++ abc269/src/bin/b.rs
@@ -1,15 +1,20 @@
 use std::fmt;
 
-use proconio::input;
+use proconio::{input, marker::Bytes};
 
 fn main() {
     input! {
-        n: i32,
+        ss: [Bytes; 10],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(&ss));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(ss: &[Vec<u8>]) -> impl fmt::Display {
+    let a = ss.iter().position(|s| s.contains(&b'#')).unwrap() + 1;
+    let b = ss.iter().rposition(|s| s.contains(&b'#')).unwrap() + 1;
+    let c = ss[a - 1].iter().position(|&c| c == b'#').unwrap() + 1;
+    let d = ss[a - 1].iter().rposition(|&c| c == b'#').unwrap() + 1;
+
+    format!("{} {}\n{} {}", a, b, c, d)
 }
