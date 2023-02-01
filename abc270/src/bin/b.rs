d080a289089ef8c1c59dfe832dd91059ad588aa0
rusconn <rusconn3@gmail.com>
2023-02-01 05:56:39 +0000

    WA abc270_b

--- abc270/src/bin/b.rs
+++ abc270/src/bin/b.rs
@@ -4,12 +4,26 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        x: i16,
+        y: i16,
+        z: i16,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(x, y, z));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(x: i16, y: i16, z: i16) -> impl fmt::Display {
+    if y < 0 || x < y {
+        return x;
+    }
+
+    if y < z {
+        return -1;
+    }
+
+    if 0 < z {
+        return x;
+    }
+
+    -2 * z + x
 }
