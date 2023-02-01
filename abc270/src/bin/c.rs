a0126b0f4467e77cd6a2175fb3fbbb971a085835
rusconn <rusconn3@gmail.com>
2023-02-01 09:11:04 +0000

    TLE abc270_c

--- abc270/src/bin/c.rs
+++ abc270/src/bin/c.rs
@@ -1,15 +1,42 @@
 use std::fmt;
 
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        x: usize,
+        y: usize,
+        uvs: [(usize, usize); n - 1],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(n, x, y, &uvs));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(n: usize, x: usize, y: usize, uvs: &[(usize, usize)]) -> impl fmt::Display {
+    let mut edges = vec![vec![]; n + 1];
+
+    for &(u, v) in uvs {
+        edges[u].push(v);
+        edges[v].push(u);
+    }
+
+    let mut stack = vec![vec![x]];
+
+    while let Some(path) = stack.pop() {
+        let &arrival = path.last().unwrap();
+
+        if arrival == y {
+            return path.iter().join(" ");
+        }
+
+        for &edge in &edges[arrival] {
+            if !path.contains(&edge) {
+                stack.push(path.iter().chain(&[edge]).copied().collect());
+            }
+        }
+    }
+
+    unreachable!()
 }
