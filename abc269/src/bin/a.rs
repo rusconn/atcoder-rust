7491b9a1129664543b6f205f7174351ba6e97852
rusconn <rusconn3@gmail.com>
2023-02-01 12:51:34 +0000

    AC abc269_a

--- abc269/src/bin/a.rs
+++ abc269/src/bin/a.rs
@@ -4,12 +4,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        a: i32,
+        b: i32,
+        c: i32,
+        d: i32,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(a, b, c, d));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(a: i32, b: i32, c: i32, d: i32) -> impl fmt::Display {
+    format!("{}\nTakahashi", (a + b) * (c - d))
 }
