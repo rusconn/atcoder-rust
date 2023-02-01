aaa499022960c052ca1ffdbbb087ddffb00e374d
rusconn <rusconn3@gmail.com>
2023-02-01 17:38:36 +0000

    Refine abc269_d

--- abc269/src/bin/d.rs
+++ abc269/src/bin/d.rs
@@ -1,14 +1,7 @@
 use std::fmt;
 
-use itertools::Itertools;
 use proconio::input;
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-enum Cell {
-    White,
-    Black,
-    Count,
-}
+use rustc_hash::FxHashSet;
 
 fn main() {
     input! {
@@ -20,44 +13,32 @@ fn main() {
 }
 
 fn solve(xys: &[(i16, i16)]) -> impl fmt::Display {
-    let xys = xys
-        .iter()
-        .map(|(x, y)| ((x + 1000) as usize, (y + 1000) as usize))
-        .collect_vec();
-
-    let mut map = vec![vec![Cell::White; 2000 + 1]; 2000 + 1];
-
-    for &(x, y) in &xys {
-        map[x][y] = Cell::Black;
-    }
-
+    let mut seen = FxHashSet::default();
     let mut count = 0;
 
-    for &(x, y) in &xys {
-        if map[x][y] == Cell::Black {
-            let mut stack = vec![(x as i16, y as i16)];
+    for &xy in xys {
+        if !seen.insert(xy) {
+            continue;
+        }
 
-            while let Some((x, y)) = stack.pop() {
-                map[x as usize][y as usize] = Cell::Count;
+        let mut stack = vec![xy];
 
-                for &(x, y) in [
-                    (x - 1, y - 1),
-                    (x - 1, y),
-                    (x, y - 1),
-                    (x, y + 1),
-                    (x + 1, y),
-                    (x + 1, y + 1),
-                ]
-                .iter()
-                .filter(|&&(x, y)| 0 <= x && x <= 2000 && 0 <= y && y <= 2000)
-                .filter(|&&(x, y)| map[x as usize][y as usize] == Cell::Black)
-                {
-                    stack.push((x, y));
+        while let Some((x, y)) = stack.pop() {
+            for &xy in &[
+                (x - 1, y - 1),
+                (x - 1, y),
+                (x, y - 1),
+                (x, y + 1),
+                (x + 1, y),
+                (x + 1, y + 1),
+            ] {
+                if xys.contains(&xy) && seen.insert(xy) {
+                    stack.push(xy);
                 }
             }
-
-            count += 1;
         }
+
+        count += 1;
     }
 
     count
