472a88d068a8bf1400fa7b086526c4b1c3071cbb
rusconn <rusconn3@gmail.com>
2023-02-04 05:39:43 +0000

    AC abc267_c

--- abc267/src/bin/c.rs
+++ abc267/src/bin/c.rs
@@ -4,12 +4,31 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        m: usize,
+        a: [i64; n],
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(m, &a));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(m: usize, a: &[i64]) -> impl fmt::Display {
+    let mut sum1: i64 = a.iter().take(m).sum();
+
+    let mut sum: i64 = a
+        .iter()
+        .take(m)
+        .enumerate()
+        .map(|(i, bi)| (i + 1) as i64 * bi)
+        .sum();
+
+    let mut max_sum = sum;
+
+    for i in 0..(a.len() - m) {
+        sum = sum - sum1 + (m as i64) * a[m + i];
+        max_sum = max_sum.max(sum);
+        sum1 = sum1 - a[i] + a[m + i];
+    }
+
+    max_sum
 }
