f58f97fbb0261e3d01dc62c17d128adca36956f3
rusconn <rusconn3@gmail.com>
2023-02-04 15:52:53 +0000

    AC abc266_b

--- abc266/src/bin/b.rs
+++ abc266/src/bin/b.rs
@@ -4,12 +4,18 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: i64,
     }
 
     println!("{}", solve(n));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(n: i64) -> impl fmt::Display {
+    let r = n % 998244353;
+
+    if r < 0 {
+        998244353 + r
+    } else {
+        r
+    }
 }
