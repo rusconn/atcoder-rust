e1b3c47101a26359684c2d4801260af20ac12453
rusconn <rusconn3@gmail.com>
2023-02-06 04:11:43 +0000

    AC abc265_a

--- abc265/src/bin/a.rs
+++ abc265/src/bin/a.rs
@@ -1,15 +1,19 @@
 use std::fmt;
 
+use num::Integer;
 use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        x: u16,
+        y: u16,
+        n: u16,
     }
 
-    println!("{}", solve(n));
+    println!("{}", solve(x, y, n));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(x: u16, y: u16, n: u16) -> impl fmt::Display {
+    let (yc, xc) = n.div_rem(&3);
+    (x * n).min(y * yc + x * xc)
 }
