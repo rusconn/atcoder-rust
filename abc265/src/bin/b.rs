ceeb53a910540791e22bc64000e0b3b010b309ee
rusconn <rusconn3@gmail.com>
2023-02-06 07:37:52 +0000

    AC abc265_b

--- abc265/src/bin/b.rs
+++ abc265/src/bin/b.rs
@@ -1,15 +1,39 @@
-use std::fmt;
+use std::{fmt, iter::FromIterator};
 
 use proconio::input;
+use rustc_hash::FxHashMap;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        m: usize,
+        t: usize,
+        a: [usize; n - 1],
+        xys: [(usize, usize); m],
     }
 
-    println!("{}", solve(n));
+    let xys = FxHashMap::from_iter(xys);
+
+    println!("{}", solve(t, &a, &xys));
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn solve(t: usize, a: &[usize], xys: &FxHashMap<usize, usize>) -> impl fmt::Display {
+    if is_reachable(t, a, xys) {
+        "Yes"
+    } else {
+        "No"
+    }
+}
+
+fn is_reachable(mut t: usize, a: &[usize], xys: &FxHashMap<usize, usize>) -> bool {
+    for (i, &ai) in a.iter().enumerate() {
+        if t <= ai {
+            return false;
+        }
+
+        t -= ai;
+        t += xys.get(&(i + 2)).unwrap_or(&0);
+    }
+
+    true
 }
