4351104a58acf95b4f5267dcdc58931a591d67bd
rusconn <rusconn3@gmail.com>
2023-06-25 11:04:14 +0000

    Refine abc264_b

--- abc264/src/bin/b.rs
+++ abc264/src/bin/b.rs
@@ -1,42 +1,22 @@
-use std::fmt;
+use std::{fmt, i8};
 
 use proconio::input;
 
-enum Quad {
-    U,
-    R,
-    D,
-    L,
-}
-
 fn main() {
     input! {
-        r: usize,
-        c: usize,
+        r: i8,
+        c: i8,
     }
 
     println!("{}", solve(r, c));
 }
 
-fn solve(r: usize, c: usize) -> impl fmt::Display {
-    let q;
+fn solve(r: i8, c: i8) -> impl fmt::Display {
+    let d = i8::max((8 - r).abs(), (8 - c).abs());
 
-    if r <= c {
-        q = if r + c <= 16 { Quad::U } else { Quad::R }
-    } else {
-        q = if r + c <= 16 { Quad::L } else { Quad::D }
-    }
-
-    let parity = match q {
-        Quad::U => r % 2,
-        Quad::R => (16 - c) % 2,
-        Quad::D => (16 - r) % 2,
-        Quad::L => c % 2,
-    };
-
-    if parity == 1 {
-        "black"
-    } else {
+    if d % 2 == 0 {
         "white"
+    } else {
+        "black"
     }
 }
