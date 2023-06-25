fabb244474e0392607c4e69bfb45e57b7191e7b7
rusconn <rusconn3@gmail.com>
2023-06-25 10:28:49 +0000

    AC abc264_b

--- abc264/src/bin/b.rs
+++ abc264/src/bin/b.rs
@@ -2,14 +2,41 @@ use std::fmt;
 
 use proconio::input;
 
-fn main() {
-    input! {
-        n: i32,
-    }
-
-    println!("{}", solve(n));
+enum Quad {
+    U,
+    R,
+    D,
+    L,
 }
 
-fn solve(n: i32) -> impl fmt::Display {
-    n
+fn main() {
+    input! {
+        r: usize,
+        c: usize,
+    }
+
+    println!("{}", solve(r, c));
+}
+
+fn solve(r: usize, c: usize) -> impl fmt::Display {
+    let q;
+
+    if r <= c {
+        q = if r + c <= 16 { Quad::U } else { Quad::R }
+    } else {
+        q = if r + c <= 16 { Quad::L } else { Quad::D }
+    }
+
+    let parity = match q {
+        Quad::U => r % 2,
+        Quad::R => (16 - c) % 2,
+        Quad::D => (16 - r) % 2,
+        Quad::L => c % 2,
+    };
+
+    if parity == 1 {
+        "black"
+    } else {
+        "white"
+    }
 }
