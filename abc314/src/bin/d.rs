31036c79c5669ddb3bf3bee31a5a24d9110bf54f
rusconn <rusconn3@gmail.com>
2023-08-13 19:18:59 +0900

    AC abc314_d

--- abc314/src/bin/d.rs
+++ abc314/src/bin/d.rs
@@ -1,11 +1,33 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-        n: i32,
+        _: usize,
+        mut s: Chars,
+        q: usize,
+        txcs: [(u8, usize, char); q],
     }
 
-    let ans = n + 1;
+    let i = txcs.iter().rposition(|x| x.0 != 1);
+    let t = i.map(|i| txcs[i].0).unwrap_or(1);
+    let i = i.unwrap_or(0);
 
-    println!("{}", ans);
+    let (first, second) = txcs.split_at(i);
+
+    for &(_, c, x) in first.iter().filter(|x| x.0 == 1) {
+        s[c - 1] = x;
+    }
+
+    if t == 2 {
+        s.iter_mut().for_each(|c| *c = c.to_ascii_lowercase());
+    }
+    if t == 3 {
+        s.iter_mut().for_each(|c| *c = c.to_ascii_uppercase());
+    };
+
+    for &(_, c, x) in second.iter().filter(|x| x.0 == 1) {
+        s[c - 1] = x;
+    }
+
+    println!("{}", s.iter().collect::<String>());
 }
