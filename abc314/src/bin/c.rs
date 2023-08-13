174b0aeb6fd0a0104ad945b12a3ddb06f574609f
rusconn <rusconn3@gmail.com>
2023-08-13 15:31:58 +0900

    AC abc314_c

--- abc314/src/bin/c.rs
+++ abc314/src/bin/c.rs
@@ -1,11 +1,33 @@
-use proconio::input;
+use std::collections::VecDeque;
+
+use proconio::{
+    input,
+    marker::{Chars, Usize1},
+};
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        m: usize,
+        s: Chars,
+        cs: [Usize1; n],
     }
 
-    let ans = n + 1;
+    let mut vs = vec![VecDeque::new(); m];
 
-    println!("{}", ans);
+    for (i, &c) in cs.iter().enumerate() {
+        vs[c].push_back(s[i]);
+    }
+
+    for v in vs.iter_mut() {
+        v.rotate_right(1);
+    }
+
+    let mut s = String::new();
+
+    for c in cs {
+        s.push(vs[c].pop_front().unwrap());
+    }
+
+    println!("{s}");
 }
