f174cc588e0f47f99ee7106af32efedaec1c7a62
rusconn <rusconn3@gmail.com>
2023-08-13 11:18:23 +0900

    AC abc314_a

--- abc314/src/bin/a.rs
+++ abc314/src/bin/a.rs
@@ -1,11 +1,14 @@
+use itertools::Itertools;
 use proconio::input;
+
+const PI: &str = "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
     }
 
-    let ans = n + 1;
+    let ans = PI.chars().take(n + 2).join("");
 
-    println!("{}", ans);
+    println!("{ans}");
 }
