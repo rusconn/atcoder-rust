5fb8cfa7c3aff73dae4ca27664047248e4f8c598
rusconn <rusconn3@gmail.com>
2024-02-22 21:20:58 +0900

    AC abc313_b

--- abc313/src/bin/b.rs
+++ abc313/src/bin/b.rs
@@ -1,11 +1,25 @@
+use std::collections::BTreeSet;
+
 use proconio::input;
 
 fn main() {
     input! {
         n: i32,
+        m: i32,
+        abs: [(i32, i32); m],
     }
 
-    let ans = n + 1;
+    let mut s = (1..=n).collect::<BTreeSet<_>>();
+
+    for (_, b) in abs {
+        s.remove(&b);
+    }
+
+    let ans = if s.len() == 1 {
+        s.pop_first().unwrap()
+    } else {
+        -1
+    };
 
     println!("{ans}");
 }
