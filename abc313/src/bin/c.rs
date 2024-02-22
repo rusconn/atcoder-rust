2b56418e17cdc27d15b5d2b32807dfea60d27805
rusconn <rusconn3@gmail.com>
2024-02-22 22:41:45 +0900

    AC abc313_c

--- abc313/src/bin/c.rs
+++ abc313/src/bin/c.rs
@@ -2,10 +2,15 @@ use proconio::input;
 
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        ai: [usize; n],
     }
 
-    let ans = n + 1;
+    let sum: usize = ai.iter().sum();
+    let mean = sum as f64 / n as f64;
+    let (meanf, meanc) = (mean.floor() as usize, mean.ceil() as usize);
+    let x: usize = ai.iter().filter(|&&a| a < meanf).map(|a| meanf - a).sum();
+    let y: usize = ai.iter().filter(|&&a| a > meanc).map(|a| a - meanc).sum();
 
-    println!("{ans}");
+    println!("{}", x.max(y));
 }
