42dab48f64fb389381957bde07cb6f1b1e47ff8c
rusconn <rusconn3@gmail.com>
2024-02-23 22:03:54 +0900

    AC abc312_b

--- abc312/src/bin/b.rs
+++ abc312/src/bin/b.rs
@@ -1,11 +1,24 @@
-use proconio::input;
+use proconio::{fastout, input};
 
+#[fastout]
 fn main() {
     input! {
-        n: i32,
+        n: usize,
+        m: usize,
+        ss: [String; n],
     }
 
-    let ans = n + 1;
-
-    println!("{ans}");
+    for i in 0..=(n - 9) {
+        for j in 0..=(m - 9) {
+            if [(0, "###."), (1, "###."), (2, "###."), (3, "....")]
+                .iter()
+                .all(|(o, s)| ss[i + o][j..j + 4] == **s)
+                && [(5, "...."), (6, ".###"), (7, ".###"), (8, ".###")]
+                    .iter()
+                    .all(|(o, s)| ss[i + o][j + 5..j + 9] == **s)
+            {
+                println!("{} {}", i + 1, j + 1);
+            }
+        }
+    }
 }
