7d878140e7445e18df4d2fa73cf105b07824f697
rusconn <rusconn3@gmail.com>
2024-02-23 09:12:08 +0900

    Refine abc313_c

--- abc313/src/bin/c.rs
+++ abc313/src/bin/c.rs
@@ -7,10 +7,10 @@ fn main() {
     }
 
     let sum: usize = ai.iter().sum();
-    let mean = sum as f64 / n as f64;
-    let (meanf, meanc) = (mean.floor() as usize, mean.ceil() as usize);
-    let x: usize = ai.iter().filter(|&&a| a < meanf).map(|a| meanf - a).sum();
-    let y: usize = ai.iter().filter(|&&a| a > meanc).map(|a| a - meanc).sum();
+    let means = sum / n;
+    let meanl = means + 1;
+    let x: usize = ai.iter().filter(|&&a| a < means).map(|a| means - a).sum();
+    let y: usize = ai.iter().filter(|&&a| a > meanl).map(|a| a - meanl).sum();
 
     println!("{}", x.max(y));
 }
